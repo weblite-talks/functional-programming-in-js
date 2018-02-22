bb4a98ca5fae7d516fa70276a4ef034dba957a48
ali <aliasgary73@gmail.com>
2018-02-22 11:42:33 +0330

    first class functions refactor

--- examples/first-class-function.js
+++ examples/first-class-function.js
@@ -1,6 +1,14 @@
-const multiply4 = a => a * 4
+const multiply3 = a => a * 3
 
-const arrayOfFuncs = [a => a * 2, a => a * 3, multiply4]
+const arrayOfFuncs = [a => a * 2, a => a * 3, multiply3]
+
+const mapOfFunc = {
+  sum: (a, b) => a + b,
+  multiply: (a, b) => a * b,
+}
 
 
-console.log(arrayOfFuncs.map(func => func(4)))
+console.log(multiply3(1))
+console.log(arrayOfFuncs[0](2))
+console.log(mapOfFunc.sum(2, 3))
+console.log(mapOfFunc.multiply(2, 3))
