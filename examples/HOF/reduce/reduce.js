2d16cf4c96c1024cc46fc3ac547a7e956e2779ef
ali <aliasgary73@gmail.com>
2018-02-23 12:27:25 +0330

    add example to reduce

--- examples/HOF/reduce/reduce.js
+++ examples/HOF/reduce/reduce.js
@@ -2,9 +2,11 @@ const R = require('ramda')
 
 
 // example 1
-const arraySum = R.reduce((acc, item) => acc + item, 0)
+const sum = (acc, item) => acc + item
+const arraySum = R.reduce(sum, 0)
 
-console.log(arraySum(R.range(1, 11)))
+console.log(arraySum(R.range(1, 4)))
+console.log(sum(sum(sum(0, 1), 2), 3))
 
 
 // example 2
