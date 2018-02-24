f823b32e874a3720e19f00be1951ff317135f3f5
ali <aliasgary73@gmail.com>
2018-02-24 16:11:30 +0330

    refactor goods

--- examples/practice/goods.js
+++ examples/practice/goods.js
@@ -1,47 +1,25 @@
 const R = require('ramda')
 
 const items = [
-  {
-    quantity: 1,
-    price: 4,
-    name: 'Some toy',
-    shipping: false
-  },
-  {
-    quantity: 3,
-    price: 8,
-    name: 'Some fancier toy',
-    shipping: false
-  },
-  {
-    quantity: 1,
-    price: 40,
-    name: 'Really fancy toy',
-    shipping: false
-  },
-  {
-    price: 5,
-    name: 'Shipping',
-    shipping: true
-  }
+  { quantity: 10,  price: 4,   name: 'potato',  type: 'fruit' },
+  { quantity: 15,  price: 4,   name: 'milk',    type: 'dairy' },
+  { quantity: 30,  price: 3,   name: 'tomato',  type: 'fruit' },
+  { quantity: 5,   price: 10,  name: 'mango',   type: 'fruit' },
+  { quantity: 25,  price: 7,   name: 'yogurt',  type: 'dairy' },
+  { quantity: 10,  price: 6,   name: 'apple',   type: 'fruit' },
+  { quantity: 20,  price: 5,   name: 'orange',  type: 'fruit' },
 ]
 
-function isShipping (item) {
-  return !item.shipping
-}
-
-function getItemPrice (item) {
-  return item.quantity * item.price
-}
-
-function getItemsTotal (total, current) {
-  return total + current
-}
+const print = total => `total price is: ${total}$`
+const isShipping = item => !item.shipping
+const getItemPrice = item => item.quantity * item.price
+const getItemsTotal = (total, current) => total + current
 
 const totalPrice = R.compose(
+  print,
   R.reduce(getItemsTotal, 0),
   R.map(getItemPrice),
   R.filter(isShipping),
 )(items)
 
-console.log(totalPrice)
+console.log(totalPrice) // => total price is: 575$
