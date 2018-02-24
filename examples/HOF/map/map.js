9f7430040e5267e26f6fc0e713902f283b7d7c96
ali <aliasgary73@gmail.com>
2018-02-24 23:37:48 +0330

    add example to map

--- examples/HOF/map/map.js
+++ examples/HOF/map/map.js
@@ -7,7 +7,26 @@ const arrayMultiplyTenEachItem = R.map(x => x * 10)
 console.log(arrayMultiplyTenEachItem(R.range(1, 11)))
 
 
+
 // example 2
 const arrayUppercaseEachItem = R.map(R.toUpper)
 
 console.log(arrayUppercaseEachItem(['weblite', 'talks']))
+
+
+
+// example 3
+const fruits = [
+  { name: 'apple', number: 2 },
+  { name: 'orange', number: 10 },
+  { name: 'tomato', number: 5 },
+  { name: 'potato', number: 1 },
+]
+
+const fruitsString = R.map(fruit => `i have ${fruit.number} ${fruit.name}`, fruits)
+
+console.log(fruitsString)
+// =>  [ 'i have 2 apple',
+//       'i have 10 orange',
+//       'i have 5 tomato',
+//       'i have 1 potato' ]
