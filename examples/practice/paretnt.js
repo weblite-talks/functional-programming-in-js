011c220f9f7fb6ad72b1a654eede7b79975ea565
ali <aliasgary73@gmail.com>
2018-02-25 10:55:12 +0330

    add familyTree to practices

---
+++ examples/practice/paretnt.js
@@ -0,0 +1,40 @@
+const R = require('ramda')
+
+const mohammadiFamily = [
+  { name: 'kazem mohammadi', parent: null },
+
+  { name: 'hossein mohammadi', parent: 'kazem mohammadi' },
+  { name: 'hasan mohammadi', parent: 'kazem mohammadi' },
+
+  { name: 'saeed mohammadi', parent: 'hossein mohammadi' },
+  { name: 'sarah mohammadi', parent: 'hossein mohammadi' },
+  { name: 'siamak mohammadi', parent: 'hossein mohammadi' },
+
+  { name: 'ali mohammadi', parent: 'hasan mohammadi' },
+  { name: 'zahra mohammadi', parent: 'hasan mohammadi' },
+]
+
+// {
+//   'kazem mohammadi': {
+//
+//     'hossein mohammadi': {
+//       'saeed mohammadi': {},
+//       'sarah mohammadi': {},
+//       'siamak mohammadi': {},
+//     },
+//
+//     'hasan mohammadi': {
+//       'ali mohammadi': {},
+//       'zahra mohammadi': {},
+//     },
+//   }
+// }
+
+const beautifulObjectLog = object => console.log(JSON.stringify(object, null, 2))
+
+const familyTree = (family, parent) => R.compose(
+  R.reduce((tree, member) => R.assoc(member.name, familyTree(family, member.name), tree), {}),
+  R.filter(member => member.parent === parent),
+)(family)
+
+beautifulObjectLog(familyTree(mohammadiFamily, null))
