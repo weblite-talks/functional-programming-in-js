eabece350a5998c85f5b2c1f6a9bec08111a277b
ali <aliasgary73@gmail.com>
2018-02-25 22:58:52 +0330

    add examples to hof

--- examples/HOF/HOF/HOF.js
+++ examples/HOF/HOF/HOF.js
@@ -19,4 +19,29 @@ console.log(uppercase('mostafa'))
 console.log(lowercase('mostafa'))
 console.log(firstLetter('mostafa'))
 
-console.log(uppercase(1))
+
+
+
+// some usefull R HOFs
+// R.find
+console.log(
+  R.find(R.propEq('name', 'masoud'), [
+    { name: 'ali' },
+    { name: 'amir' },
+    { name: 'mostafa' },
+    { name: 'masoud' },
+    { name: 'amin' },
+  ]),
+) // => { name: 'masoud' }
+
+
+
+
+// R.where
+const predicate = R.where({
+  a: R.equals('foo'),
+  x: R.gt(10),
+  y: R.lt(20)
+})
+console.log(predicate({a: 'foo', x: 9, y: 21})) // => true
+console.log(predicate({a: 'bar', x: 11, y: 19})) // => false
