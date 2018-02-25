51c0c0bcd580652c0bb4910f00cef3a0ec776544
ali <aliasgary73@gmail.com>
2018-02-25 22:40:14 +0330

    refrencial transparency

--- examples/refrencial-trancparency/refrencial-transparency.js
+++ examples/refrencial-trancparency/refrencial-transparency.js
@@ -1,7 +1,10 @@
+const R = require('ramda')
+
+
 const add = (a, b) => a + b // pure
 
 const addWithLog = (a, b) => { // not pure
-  const dnumber = Math.random()
+  const random = Math.random()
 
   return a + b + random
 }
@@ -15,3 +18,11 @@ const addWithSomeAjaxCall = (a, b) => { // not pure
 add(1, 4) // 5
 addWithLog(1, 4) // ?
 addWithSomeAjaxCall(3, 6) // ?
+
+
+
+
+// R
+console.log(R.remove(1, 3, [1, 2, 3, 4])) // => [ 1 ]
+
+console.log(R.reverse([1, 2, 3, 4])) // => [ 4, 3, 2, 1 ]
