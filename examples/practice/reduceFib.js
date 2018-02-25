6d663a438cc2ac0f349cfbc34879fff43835a33f
ali <aliasgary73@gmail.com>
2018-02-26 02:49:46 +0330

    reduceFib example

---
+++ examples/practice/reduceFib.js
@@ -0,0 +1,18 @@
+const R = require('ramda')
+
+// R.reduce
+// R.repeat
+// R.head
+// R.last
+
+
+const fibSeq = n => R.compose(
+  list => n === 1 ? R.head(list) : R.last(list),
+  n => R.reduce(numbers => ([numbers[1], numbers[0] + numbers[1]]), [0, 1], R.repeat(null, n)),
+  n => (n >= 2) ? n - 2 : 0,
+)(n)
+
+
+
+console.log(fibSeq(5)) // => 3
+// 0 1 1 2 3 5 8 ...
