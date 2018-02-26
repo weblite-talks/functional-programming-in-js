03e8b5587deeab3773ca32c0da52c5af030e5e5f
ali <aliasgary73@gmail.com>
2018-02-26 14:10:21 +0330

    refactor

--- examples/practice/series.js
+++ examples/practice/series.js
@@ -7,17 +7,28 @@ const R = require('ramda')
 
 
 
-const series1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const naturalSeries = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+
+
+console.log(R.compose(
+  R.map(R.multiply(2)),
+  R.filter(number => number % 2 === 0),
+)(naturalSeries))// => [4, 8, 12, 16, 20]
+
+
 
 
 console.log(R.compose(
   R.map(number => (number * 2) + 1),
   R.filter(number => number % 2 !== 0),
-)(series1))// => [3, 7, 11, 15, 19]
+)(naturalSeries))// => [3, 7, 11, 15, 19]
+
 
 
 
 console.log(R.compose(
   R.map(number => (number * 3) + 1),
   R.filter(number => number % 3 === 0),
-)(series1)) // => [10, 19, 28]
+)(naturalSeries)) // => [10, 19, 28]
